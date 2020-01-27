a76a478176097361bd13b8e6b7bd60877e4f5ec3
Chris <chris@ladybug.tools>
2020-01-26 22:32:07 -0500

    feat(door): Allow Shades to be assigned to Doors
    This commit brings the library in line with [this PR on honeybee-core](https://github.com/ladybug-tools/honeybee-core/pull/52).

--- honeybee_grasshopper_energy/src/HB Apply Shade Construction.py
+++ honeybee_grasshopper_energy/src/HB Apply Shade Construction.py
@@ -9,21 +9,21 @@
 
 """
 Apply a ShadeConstruction to Honeybee Shade objects. Alternatively, it can
-assign a ShadeConstruction to all of the child shades of an Aperture, Face,
-or a Room.
+assign a ShadeConstruction to all of the child shades of an Aperture, Door,
+Face, or a Room.
 _
 This component supports the assigning of different constructions based on cardinal
 orientation, provided that a list of ShadeConstructions are input to the _constr. 
 -
 
     Args:
-        _hb_objs: Honeybee Shades, Apertures, Faces, or Rooms to which the input
-            _constr should be assigned. For the case of a Honeybee Aperture,
-            Face or Room, the ShadeConstruction will be assigned to only the
+        _hb_objs: Honeybee Shades, Apertures, Doors, Faces, or Rooms to which the
+            input _constr should be assigned. For the case of a Honeybee Aperture,
+            Door, Face or Room, the ShadeConstruction will be assigned to only the
             child shades directly assigned to that object. So passing in a Room
             will not change the construction of shades assigned to Apertures
             of the Room's Faces. If this is the desired outcome, then the Room
-            should be deconstructed into its child apertures before using
+            should be deconstructed into its child objects before using
             this component.
         _constr: A Honeybee ShadeConstruction to be applied to the input _hb_objs.
             This can also be text for a construction to be looked up in the shade
@@ -37,7 +37,7 @@ orientation, provided that a list of ShadeConstructions are input to the _constr
 
 ghenv.Component.Name = "HB Apply Shade Construction"
 ghenv.Component.NickName = 'ApplyShadeConstr'
-ghenv.Component.Message = '0.1.0'
+ghenv.Component.Message = '0.2.0'
 ghenv.Component.Category = 'Energy'
 ghenv.Component.SubCategory = '1 :: Constructions'
 ghenv.Component.AdditionalHelpFromDocStrings = '3'
@@ -53,6 +53,7 @@ try:  # import the core honeybee dependencies
     from honeybee.room import Room
     from honeybee.face import Face
     from honeybee.aperture import Aperture
+    from honeybee.door import Door
     from honeybee.orientation import angles_from_num_orient, face_orient_index
 except ImportError as e:
     raise ImportError('\nFailed to import honeybee:\n\t{}'.format(e))
@@ -73,14 +74,14 @@ if all_required_inputs(ghenv.Component):
             _constr[i] = shade_construction_by_name(constr)
     
     # error message for unrecognized object
-    error_msg = 'Input _hb_objs must be a Room, Face, Aperture, or Shade. Not {}.'
+    error_msg = 'Input _hb_objs must be a Room, Face, Aperture, Door, or Shade. Not {}.'
     
     # assign the constructions
     if len(_constr) == 1:
         for obj in hb_objs:
             if isinstance(obj, Shade):
                 obj.properties.energy.construction = _constr[0]
-            elif isinstance(obj, (Aperture, Face, Room)):
+            elif isinstance(obj, (Aperture, Face, Room, Door)):
                 for shd in obj.shades:
                     shd.properties.energy.construction = _constr[0]
             else:
@@ -88,7 +89,7 @@ if all_required_inputs(ghenv.Component):
     else:  # assign constructions based on cardinal direction
         angles = angles_from_num_orient(len(_constr))
         for obj in hb_objs:
-            if isinstance(obj, (Aperture, Face)):
+            if isinstance(obj, (Aperture, Face, Door)):
                 orient_i = face_orient_index(obj, angles)
                 if orient_i is not None:
                     for shd in obj.shades:
