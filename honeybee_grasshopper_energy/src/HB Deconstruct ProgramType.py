1fe0250fbb0212372da2b4089ffbe483ec017888
Chris Mackey <chris@mackeyarchitecture.com>
2021-06-29 22:10:45 -0400

    fix(program): Add support for assigning mixed building programs

--- honeybee_grasshopper_energy/src/HB Deconstruct ProgramType.py
+++ honeybee_grasshopper_energy/src/HB Deconstruct ProgramType.py
@@ -39,13 +39,14 @@ Deconstruct a ProgramType object into its component load objects.
 
 ghenv.Component.Name = "HB Deconstruct ProgramType"
 ghenv.Component.NickName = 'DeconstrProgram'
-ghenv.Component.Message = '1.2.0'
+ghenv.Component.Message = '1.2.1'
 ghenv.Component.Category = 'HB-Energy'
 ghenv.Component.SubCategory = '3 :: Loads'
 ghenv.Component.AdditionalHelpFromDocStrings = "1"
 
 try:
-    from honeybee_energy.lib.programtypes import program_type_by_identifier
+    from honeybee_energy.lib.programtypes import program_type_by_identifier, \
+        building_program_type_by_identifier
 except ImportError as e:
     raise ImportError('\nFailed to import honeybee_energy:\n\t{}'.format(e))
 
@@ -58,7 +59,10 @@ except ImportError as e:
 if all_required_inputs(ghenv.Component):
     # get the program from the library if it is a identifier
     if isinstance(_program, str):
-        _program = program_type_by_identifier(_program)
+        try:
+            _program = building_program_type_by_identifier(_program)
+        except ValueError:
+            _program = program_type_by_identifier(_program)
 
     # get the components of the program
     people = _program.people
