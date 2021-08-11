2dc48886728235ab73493f5f47be1abc47a274ec
Chris Mackey <chris@mackeyarchitecture.com>
2021-08-11 14:53:47 -0700

    fix(simulate): Improve Solar Distribution Docstring

--- honeybee_grasshopper_energy/src/HB Shadow Calculation.py
+++ honeybee_grasshopper_energy/src/HB Shadow Calculation.py
@@ -18,44 +18,47 @@ Create settings for the EnergyPlus Shadow Calculation.
             * 0 = "MinimalShadowing" - In this case, exterior shadowing is only computed
                 for windows and not for other opaque surfaces that might have their
                 surface temperature affected by the sun. All beam solar radiation
-                entering the zone is assumed to fall on the floor. A simple window
-                view factor calculation will be used to distribute incoming diffuse
+                entering the room is assumed to fall on the floor. A simple window
+                view factor calculation is used to distribute incoming diffuse
                 solar energy between interior surfaces.
             * 1 = "FullExterior" - The simulation will perform the solar calculation
                 in a manner that only accounts for direct sun and whether it is
                 blocked by surrounding context geometry.  For the inside of the
-                building, all beam solar radiation entering the zone is assumed
-                to fall on the floor. A simple window view factor calculation will
-                be used to distribute incoming diffuse solar energy between
+                building, all beam solar radiation entering the room is assumed
+                to fall on the floor. A simple window view factor calculation is
+                used to distribute incoming diffuse solar energy between
                 interior surfaces.
             * 2 = "FullInteriorAndExterior" - The simulation will perform the solar
                 calculation in a manner that models the direct sun (and wheter it
                 is blocked by outdoor context goemetry.  It will also ray trace
-                the direct sun on the interior of zones to distribute it correctly
+                the direct sun on the interior of rooms to distribute it correctly
                 between interior surfaces.  Any indirect sun or sun bouncing off
-                of objects will not be modled.
-            * 3 = "FullExteriorWithReflections" - The simulation will perform the
+                of objects will not be modled. Note that, if you use this
+                method without setting the _calc_method_ to PixelCounting,
+                EnergyPlus will give Severe warnings if your rooms have concave
+                geometry (aka. are "L"-shaped). So it is recommended that this
+                solar distribution only be used with the PixelCounting.
+            * 3 = "FullExteriorWithReflections" - [DEFAULT] The simulation will perform the
                 solar calculation in a manner that accounts for both direct sun
                 and the light bouncing off outdoor surrounding context.  For the
-                inside of the building, all beam solar radiation entering the zone
+                inside of the building, all beam solar radiation entering the room
                 is assumed to fall on the floor. A simple window view factor
-                calculation will be used to distribute incoming diffuse solar
+                calculation is used to distribute incoming diffuse solar
                 energy between interior surfaces.
-            * 4 = "FullInteriorAndExteriorWithReflections" - The simulation will
-                perform the solar calculation in a manner that accounts for light
-                bounces that happen both outside and inside the zones.  This is the
-                most accurate method and is the one assigned by default.  Note that,
-                if you use this method, EnergyPlus will give Severe warnings if
-                your zones have concave geometry (aka. are "L"-shaped).  Such
-                geometries are not supported by this solar distribution calculation
-                and it is recommeded that you either break up your zones in this
-                case or not use this solar distribution method.
+            * 4 = "FullInteriorAndExteriorWithReflections" - The simulation will perform
+                the solar calculation in a manner that accounts for light bounces
+                that happen both outside and inside the rooms. This is the most
+                accurate method but will take longer to run. Note that, if you
+                use this method without setting the _calc_method_ to PixelCounting,
+                EnergyPlus will give Severe warnings if your rooms have concave
+                geometry (aka. are "L"-shaped). So it is recommended that this
+                solar distribution only be used with the PixelCounting.
         _calc_method_: Text noting whether CPU-based polygon clipping method or GPU-based
             pixel counting method should be used. For low numbers of shading
             surfaces (less than ~200), PolygonClipping requires less runtime than
             PixelCounting. However, PixelCounting runtime scales significantly
             better at higher numbers of shading surfaces. PixelCounting also has
-            no limitations related to zone concavity when used with any
+            no limitations related to room concavity when used with any
             “FullInterior” solar distribution options. (Default: PolygonClipping).
             Choose from the following:
                 * PolygonClipping
@@ -80,7 +83,7 @@ Create settings for the EnergyPlus Shadow Calculation.
 
 ghenv.Component.Name = 'HB Shadow Calculation'
 ghenv.Component.NickName = 'ShadowCalc'
-ghenv.Component.Message = '1.2.1'
+ghenv.Component.Message = '1.2.2'
 ghenv.Component.Category = 'HB-Energy'
 ghenv.Component.SubCategory = '5 :: Simulate'
 ghenv.Component.AdditionalHelpFromDocStrings = '3'
