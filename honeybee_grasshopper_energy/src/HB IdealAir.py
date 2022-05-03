8e887c55382e1ed35b344dcabfe6110b990e0de7
Chris Mackey <chris@ladybug.tools>
2022-05-03 14:07:35 -0400

    fix(hvac): Expose availability schedules on Ideal Air component
    See here:
    
    https://discourse.ladybug.tools/t/heating-cooling-availability-schedules-for-hvac-systems/17315/7

--- honeybee_grasshopper_energy/src/HB IdealAir.py
+++ honeybee_grasshopper_energy/src/HB IdealAir.py
@@ -19,39 +19,47 @@ Apply a customized IdealAirSystem to Honeybee Rooms.
             the ideal air system. Economizers will mix in a greater amount of
             outdoor air to cool the zone (rather than running the cooling system)
             when the zone needs cooling and the outdoor air is cooler than the zone.
-            Choose from the options below. Default: DifferentialDryBulb.
+            Choose from the options below. (Default: DifferentialDryBulb).
                 * NoEconomizer
                 * DifferentialDryBulb
                 * DifferentialEnthalpy
         dcv_: Boolean to note whether demand controlled ventilation should be
             used on the system, which will vary the amount of ventilation air
-            according to the occupancy schedule of the zone. Default: False.
+            according to the occupancy schedule of the zone. (Default: False).
         sensible_hr_: A number between 0 and 1 for the effectiveness of sensible
-            heat recovery within the system. Default: 0.
+            heat recovery within the system. (Default: 0).
         latent_hr_: A number between 0 and 1 for the effectiveness of latent heat
-            recovery within the system. Default: 0.
+            recovery within the system. (Default: 0).
         _heat_temp_: A number for the maximum heating supply air temperature
-            [C]. Default: 50, which is typical for many air-based HVAC systems.
+            [C]. (Default: 50; suitable for most air-based HVAC systems).
         _cool_temp_: A number for the minimum cooling supply air temperature
-            [C]. Default: 13, which is typical for many air-based HVAC systems.
+            [C]. (Default: 13; sutiable for most air-based HVAC systems).
         _heat_limit_: A number for the maximum heating capacity in Watts. This
             can also be the text 'autosize' to indicate that the capacity should
             be determined during the EnergyPlus sizing calculation. This can also
             be the text 'NoLimit' to indicate no upper limit to the heating
-            capacity. Default: 'autosize'.
+            capacity. (Default: 'autosize').
         _cool_limit_: A number for the maximum cooling capacity in Watts. This
             can also be the text 'autosize' to indicate that the capacity should
             be determined during the EnergyPlus sizing calculation. This can also
             be the text 'NoLimit' to indicate no upper limit to the cooling
-            capacity. Default: 'autosize'.
-    
+            capacity. (Default: 'autosize').
+        heat_avail_: An optional on/off schedule to set the availability of
+            heating over the course of the simulation. This can also be the
+            identifier of an on/off schedule to be looked up in the schedule
+            library (Default: None).
+        cool_avail_: An optional on/off schedule to set the availability of
+            cooling over the course of the simulation. This can also be the
+            identifier of an on/off schedule to be looked up in the schedule
+            library (Default: None).
+
     Returns:
         rooms: The input Rooms with their Ideal Air Systems edited.
 """
 
 ghenv.Component.Name = "HB IdealAir"
 ghenv.Component.NickName = 'IdealAir'
-ghenv.Component.Message = '1.4.1'
+ghenv.Component.Message = '1.4.2'
 ghenv.Component.Category = 'HB-Energy'
 ghenv.Component.SubCategory = '4 :: HVAC'
 ghenv.Component.AdditionalHelpFromDocStrings = '1'
@@ -65,6 +73,7 @@ except ImportError as e:
 
 try:  # import the honeybee-energy extension
     from honeybee_energy.hvac.idealair import IdealAirSystem
+    from honeybee_energy.lib.schedules import schedule_by_identifier
 except ImportError as e:
     raise ImportError('\nFailed to import honeybee_energy:\n\t{}'.format(e))
 
@@ -90,6 +99,12 @@ if all_required_inputs(ghenv.Component):
             rooms.extend([room.duplicate() for room in hb_obj.rooms])
         else:
             rooms.append(hb_obj.duplicate())
+
+    # get schedules by identifer if they are strings
+    if isinstance(heat_avail_, str):
+        heat_avail_ = schedule_by_identifier(heat_avail_)
+    if isinstance(cool_avail_, str):
+        cool_avail_ = schedule_by_identifier(cool_avail_)
 
     # loop through the rooms and adjust their properties
     hvac_count = 0
@@ -121,6 +136,10 @@ if all_required_inputs(ghenv.Component):
                 new_ideal_air.cooling_limit = alt_numbers[_cool_limit_]
             except KeyError:
                 new_ideal_air.cooling_limit = _cool_limit_
+            if heat_avail_ is not None:
+                new_ideal_air.heating_availability = heat_avail_
+            if cool_avail_ is not None:
+                new_ideal_air.cooling_availability = cool_avail_
 
             # assign the HVAC to the Room
             room.properties.energy.hvac = new_ideal_air
