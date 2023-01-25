91504779cda9b0e39fd730e867de7de2f6f48634
Chris Mackey <chris@ladybug.tools>
2023-01-24 19:06:18 -0500

    fix(loads): Fix typo to align with core library fix

--- honeybee_grasshopper_energy/src/HB Apply Abolute Load Values.py
+++ honeybee_grasshopper_energy/src/HB Apply Abolute Load Values.py
@@ -8,9 +8,9 @@
 # @license AGPL-3.0-or-later <https://spdx.org/licenses/AGPL-3.0-or-later>
 
 """
-Apply abolute load values to Rooms.
+Apply absolute load values to Rooms.
 _
-Note that, while the assigned load values are abolute, this component will convert
+Note that, while the assigned load values are absolute, this component will convert
 them to the "normalized" value for each room (eg. lighting per floor area) in
 order to apply them to the rooms. So any existing specification of load intensity
 is overwritten with the absolute load here.
@@ -36,7 +36,7 @@ varies over the simulation, the "Always On" schedule will be used as a default.
             room in liters per hour (L/h).
         infiltration_ach_: A number for the infiltration flow rate in air changes
             per hour (ACH).
-        vent_flow_: A numerical value for the abolute of flow of outdoor air ventilation
+        vent_flow_: A numerical value for the absolute of flow of outdoor air ventilation
             for the room in cubic meters per second (m3/s). Note that inputting
             a value here will overwrite all specification of outdoor air ventilation
             currently on the room (per_floor, per_person, ach).
@@ -48,7 +48,7 @@ varies over the simulation, the "Always On" schedule will be used as a default.
 
 ghenv.Component.Name = 'HB Apply Abolute Load Values'
 ghenv.Component.NickName = 'AbsoluteLoadVals'
-ghenv.Component.Message = '1.6.0'
+ghenv.Component.Message = '1.6.1'
 ghenv.Component.Category = 'HB-Energy'
 ghenv.Component.SubCategory = '3 :: Loads'
 ghenv.Component.AdditionalHelpFromDocStrings = '2'
@@ -67,40 +67,40 @@ if all_required_inputs(ghenv.Component):
     # assign the person_count_
     if len(person_count_) != 0:
         for i, room in enumerate(rooms):
-            room.properties.energy.abolute_people(
+            room.properties.energy.absolute_people(
                 longest_list(person_count_, i), conversion)
 
     # assign the lighting_watts_
     if len(lighting_watts_) != 0:
         for i, room in enumerate(rooms):
-            room.properties.energy.abolute_lighting(
+            room.properties.energy.absolute_lighting(
                 longest_list(lighting_watts_, i), conversion)
 
     # assign the electric_watts_
     if len(electric_watts_) != 0:
         for i, room in enumerate(rooms):
-            room.properties.energy.abolute_electric_equipment(
+            room.properties.energy.absolute_electric_equipment(
                 longest_list(electric_watts_, i), conversion)
 
     # assign the gas_watts_
     if len(gas_watts_) != 0:
         for i, room in enumerate(rooms):
-            room.properties.energy.abolute_gas_equipment(
+            room.properties.energy.absolute_gas_equipment(
                 longest_list(gas_watts_, i), conversion)
 
     # assign the hot_wtr_flow_
     if len(hot_wtr_flow_) != 0:
         for i, room in enumerate(rooms):
-            room.properties.energy.abolute_service_hot_water(
+            room.properties.energy.absolute_service_hot_water(
                 longest_list(hot_wtr_flow_, i), conversion)
 
     # assign the infiltration_ach_
     if len(infiltration_ach_) != 0:
         for i, room in enumerate(rooms):
-            room.properties.energy.abolute_infiltration_ach(
+            room.properties.energy.absolute_infiltration_ach(
                 longest_list(infiltration_ach_, i), conversion)
 
     # assign the vent_flow_
     if len(vent_flow_) != 0:
         for i, room in enumerate(rooms):
-            room.properties.energy.abolute_ventilation(longest_list(vent_flow_, i))
+            room.properties.energy.absolute_ventilation(longest_list(vent_flow_, i))
