6c14405e1c7483b063631c8ffd6e804bdc6ee569
Chris Mackey <chris@ladybug.tools>
2025-04-15 20:27:39 -0700

    feat(simulate): Change Model to OSM component to use honeybee-openstudio

--- honeybee_grasshopper_energy/src/HB Model to OSM.py
+++ honeybee_grasshopper_energy/src/HB Model to OSM.py
@@ -40,21 +40,17 @@ to an IDF file and then run through EnergyPlus.
             .idf in EnergyPlus. This will ensure that all result files appear
             in their respective outputs from this component.
             _
-            This input can also be the integer "2", which will only translate the
-            honeybee jsons to an osm and idf format without running the model
-            through EnergyPlus.
-            _
-            It can also be the integer "3", which will run the whole translation
+            This input can also be the integer "2", which will run the whole translation
             and simulation silently (without any batch windows).
 
     Returns:
         report: Check here to see a report of the EnergyPlus run.
         jsons: The file paths to the honeybee JSON files that describe the Model and
             SimulationParameter. These will be translated to an OpenStudio model.
-        osw: File path to the OpenStudio Workflow JSON on this machine. This workflow
-            is executed using the OpenStudio command line interface (CLI) and
-            it includes measures to translate the Honeybee model JSON as well
-            as any other connected measures_.
+        osw: File path to the OpenStudio Workflow JSON on this machine (if necessary
+            for simulation). This workflow is executed using the OpenStudio
+            command line interface (CLI) and it includes any connected
+            measures_. Will be None if no OSW is needed for the simulation.
         osm: The file path to the OpenStudio Model (OSM) that has been generated
             on this computer.
         idf: The file path of the EnergyPlus Input Data File (IDF) that has been
@@ -74,37 +70,43 @@ to an IDF file and then run through EnergyPlus.
 
 ghenv.Component.Name = 'HB Model to OSM'
 ghenv.Component.NickName = 'ModelToOSM'
-ghenv.Component.Message = '1.8.1'
+ghenv.Component.Message = '1.8.2'
 ghenv.Component.Category = 'HB-Energy'
 ghenv.Component.SubCategory = '5 :: Simulate'
 ghenv.Component.AdditionalHelpFromDocStrings = '1'
 
-import sys
 import os
 import re
 import json
+import subprocess
 
 try:
-    from ladybug.futil import preparedir, nukedir
+    from ladybug.futil import preparedir, nukedir, copy_file_tree
     from ladybug.epw import EPW
+    from ladybug.stat import STAT
 except ImportError as e:
     raise ImportError('\nFailed to import ladybug:\n\t{}'.format(e))
 
 try:
-    import honeybee.config as hb_config
+    from honeybee.config import folders
     from honeybee.model import Model
 except ImportError as e:
     raise ImportError('\nFailed to import honeybee:\n\t{}'.format(e))
 
 try:
     from honeybee_energy.simulation.parameter import SimulationParameter
-    from honeybee_energy.run import to_openstudio_osw, run_osw, run_idf, \
-        output_energyplus_files
+    from honeybee_energy.measure import Measure
+    from honeybee_energy.run import to_openstudio_sim_folder, run_osw, run_idf, \
+        output_energyplus_files, _parse_os_cli_failure
     from honeybee_energy.result.err import Err
-    from honeybee_energy.result.osw import OSW
     from honeybee_energy.config import folders as energy_folders
 except ImportError as e:
     raise ImportError('\nFailed to import honeybee_energy:\n\t{}'.format(e))
+
+try:
+    from honeybee_openstudio.openstudio import OSModel
+except (ImportError, AssertionError):  # Openstudio C# bindings are not usable
+    OSModel = None
 
 try:
     from lbt_recipes.version import check_openstudio_version
@@ -117,6 +119,40 @@ try:
 except ImportError as e:
     raise ImportError('\nFailed to import ladybug_rhino:\n\t{}'.format(e))
 
+ROOM_COUNT_THRESH = 1000  # threshold at which the CLI is used for translation
+
+
+def measures_to_folder(measures, sim_folder):
+    osw_dict = {}  # dictionary that will be turned into the OSW JSON
+    osw_dict['steps'] = []
+    mea_folder = os.path.join(sim_folder, 'measures')
+    # ensure measures are correctly ordered
+    m_dict = {'ModelMeasure': [], 'EnergyPlusMeasure': [], 'ReportingMeasure': []}
+    for measure in measures:
+        assert isinstance(measure, Measure), 'Expected honeybee-energy Measure. ' \
+            'Got {}.'.format(type(measure))
+        m_dict[measure.type].append(measure)
+    sorted_measures = m_dict['ModelMeasure'] + m_dict['EnergyPlusMeasure'] + \
+        m_dict['ReportingMeasure']
+    # add the measures and the measure paths to the OSW
+    for measure in sorted_measures:
+        measure.validate()  # ensure that all required arguments have values
+        osw_dict['steps'].append(measure.to_osw_dict())  # add measure to workflow
+        dest_folder = os.path.join(mea_folder, os.path.basename(measure.folder))
+        copy_file_tree(measure.folder, dest_folder)
+        test_dir = os.path.join(dest_folder, 'tests')
+        if os.path.isdir(test_dir):
+            nukedir(test_dir, rmdir=True)
+    # write the dictionary to a workflow.osw
+    osw_json = os.path.join(mea_folder, 'workflow.osw')
+    try:
+        with open(osw_json, 'w') as fp:
+            json.dump(osw_dict, fp, indent=4)
+    except UnicodeDecodeError:  # non-unicode character in the dictionary
+        with open(osw_json, 'w') as fp:
+            json.dump(osw_dict, fp, indent=4, ensure_ascii=False)
+    return mea_folder
+
 
 if all_required_inputs(ghenv.Component) and _write:
     # check the presence of openstudio and check that the version is compatible
@@ -126,21 +162,21 @@ if all_required_inputs(ghenv.Component) and _write:
 
     # process the simulation parameters
     if _sim_par_ is None:
-        _sim_par_ = SimulationParameter()
-        _sim_par_.output.add_zone_energy_use()
-        _sim_par_.output.add_hvac_energy_use()
-        _sim_par_.output.add_electricity_generation()
+        sim_par = SimulationParameter()
+        sim_par.output.add_zone_energy_use()
+        sim_par.output.add_hvac_energy_use()
+        sim_par.output.add_electricity_generation()
     else:
-        _sim_par_ = _sim_par_.duplicate()  # ensure input is not edited
+        sim_par = _sim_par_.duplicate()  # ensure input is not edited
 
     # assign design days from the DDY next to the EPW if there are None
-    if len(_sim_par_.sizing_parameter.design_days) == 0:
+    if len(sim_par.sizing_parameter.design_days) == 0:
         msg = None
         folder, epw_file_name = os.path.split(_epw_file)
         ddy_file = os.path.join(folder, epw_file_name.replace('.epw', '.ddy'))
         if os.path.isfile(ddy_file):
             try:
-                _sim_par_.sizing_parameter.add_from_ddy_996_004(ddy_file)
+                sim_par.sizing_parameter.add_from_ddy_996_004(ddy_file)
             except AssertionError:
                 msg = 'No ddy_file_ was input into the _sim_par_ sizing ' \
                     'parameters\n and no design days were found in the .ddy file '\
@@ -152,39 +188,21 @@ if all_required_inputs(ghenv.Component) and _write:
             epw_obj = EPW(_epw_file)
             des_days = [epw_obj.approximate_design_day('WinterDesignDay'),
                         epw_obj.approximate_design_day('SummerDesignDay')]
-            _sim_par_.sizing_parameter.design_days = des_days
+            sim_par.sizing_parameter.design_days = des_days
             msg = msg + '\nDesign days were generated from the input _epw_file but this ' \
                 '\nis not as accurate as design days from DDYs distributed with the EPW.'
             give_warning(ghenv.Component, msg)
             print(msg)
+    if sim_par.sizing_parameter.climate_zone is None:
+        stat_file = os.path.join(folder, epw_file_name.replace('.epw', '.stat'))
+        if os.path.isfile(stat_file):
+            stat_obj = STAT(stat_file)
+            sim_par.sizing_parameter.climate_zone = stat_obj.ashrae_climate_zone
 
     # process the simulation folder name and the directory
-    _folder_ = hb_config.folders.default_simulation_folder if _folder_ is None else _folder_
+    _folder_ = folders.default_simulation_folder if _folder_ is None else _folder_
     clean_name = re.sub(r'[^.A-Za-z0-9_-]', '_', _model.display_name)
     directory = os.path.join(_folder_, clean_name, 'openstudio')
-
-    # duplicate model to avoid mutating it as we edit it for energy simulation
-    _model = _model.duplicate()
-    # scale the model if the units are not meters
-    if _model.units != 'Meters':
-        _model.convert_to_units('Meters')
-    # remove degenerate geometry within native E+ tolerance of 0.01 meters
-    try:
-        _model.remove_degenerate_geometry(0.01)
-    except ValueError:
-        error = 'Failed to remove degenerate Rooms.\nYour Model units system is: {}. ' \
-            'Is this correct?'.format(units_system())
-        raise ValueError(error)
-    # remove the HVAC from any Rooms lacking setpoints
-    rem_msgs = _model.properties.energy.remove_hvac_from_no_setpoints()
-    if rem_msgs is not None and len(rem_msgs) != 0:
-        msg = '\n'.join(rem_msgs)
-        give_warning(ghenv.Component, msg)
-        print(msg)
-
-    # auto-assign stories if there are none since most OpenStudio measures need these
-    if len(_model.stories) == 0 and len(_model.rooms) != 0:
-        _model.assign_stories_by_floor_height()
 
     # delete any existing files in the directory and prepare it for simulation
     nukedir(directory, True)
@@ -192,91 +210,93 @@ if all_required_inputs(ghenv.Component) and _write:
     sch_directory = os.path.join(directory, 'schedules')
     preparedir(sch_directory)
 
-    # write the model parameter JSONs
-    model_dict = _model.to_dict(triangulate_sub_faces=True)
-    _model.properties.energy.add_autocal_properties_to_dict(model_dict)
+    # write the model and simulation parameter to JSONs
     model_json = os.path.join(directory, '{}.hbjson'.format(clean_name))
-    if (sys.version_info < (3, 0)):  # we need to manually encode it as UTF-8
-        with open(model_json, 'wb') as fp:
-            model_str = json.dumps(model_dict, ensure_ascii=False)
-            fp.write(model_str.encode('utf-8'))
-    else:
-        with open(model_json, 'w', encoding='utf-8') as fp:
-            model_str = json.dump(model_dict, fp, ensure_ascii=False)
-
-    # write the simulation parameter JSONs
-    sim_par_dict = _sim_par_.to_dict()
+    with open(model_json, 'wb') as fp:
+        model_str = json.dumps(_model.to_dict(), ensure_ascii=False)
+        fp.write(model_str.encode('utf-8'))
     sim_par_json = os.path.join(directory, 'simulation_parameter.json')
     with open(sim_par_json, 'w') as fp:
-        json.dump(sim_par_dict, fp)
-
-    # process any measures input to the component
-    measures = None if len(measures_) == 0 or measures_[0] is None else measures_
-    no_report_meas = True if measures is None else \
-        all(meas.type != 'ReportingMeasure' for meas in measures)
-    str_inject = None if no_report_meas or add_str_ == [] or add_str_[0] is None \
-        else '\n'.join(add_str_)
-
-    # collect the two jsons for output and write out the osw file
+        json.dump(sim_par.to_dict(), fp)
     jsons = [model_json, sim_par_json]
-    osw = to_openstudio_osw(
-        directory, model_json, sim_par_json, additional_measures=measures,
-        epw_file=_epw_file, schedule_directory=sch_directory,
-        strings_to_inject=str_inject)
 
-    # run the measure to translate the model JSON to an openstudio measure
-    silent = True if run_ == 3 else False
-    if run_ > 0 and not no_report_meas:  # everything must run with OS CLI
-        if run_ == 1:  # simulate everything at once
-            osm, idf = run_osw(osw, measures_only=False, silent=silent)
-            sql, zsz, rdd, html, err = output_energyplus_files(os.path.dirname(idf))
-        else:  # remove reporting measure and give a warning
-            m_to_remove = [m.identifier for m in measures if m.type == 'ReportingMeasure']
-            with open(osw, 'r') as op:
-                osw_data = json.load(op)
-            s_to_remove = []
-            for i, step in enumerate(osw_data['steps']):
-                if step['measure_dir_name'] in m_to_remove:
-                    s_to_remove.append(i)
-            for i in reversed(s_to_remove):
-                osw_data['steps'].pop(i)
-            with open(osw, 'wb') as fp:
-                workflow_str = json.dumps(osw_data, indent=4, ensure_ascii=False)
-                fp.write(workflow_str.encode('utf-8'))
-            msg = 'The following were reporting measures and were not\n' \
-                'included in the OSW to avoid running the simulation:\n{}'.format(
-                    '\n'.join(m_to_remove))
-            give_warning(ghenv.Component, msg)
-            print(msg)
-            osm, idf = run_osw(osw, silent=silent)
-    elif run_ > 0:  # no reporting measure; simulate separately from measure application
-        osm, idf = run_osw(osw, silent=silent)
-        # process the additional strings
-        if len(add_str_) != 0 and add_str_[0] is not None and idf is not None:
+    # determine whether to run the translation with cPython or IronPython
+    use_ironpython = False
+    if OSModel is not None:
+        vent_sim_control = _model.properties.energy.ventilation_simulation_control
+        if vent_sim_control.vent_control_type == 'SingleZone':
+            if len(_model.rooms) < ROOM_COUNT_THRESH:
+                osc_version = tuple(int(v) for v in OSModel().version().str().split('.'))
+                if osc_version == energy_folders.openstudio_version:
+                    use_ironpython = True
+
+    if use_ironpython:  # translate the model using IronPython methods
+        add_str = '\n'.join(add_str_) if len(add_str_) != 0 and \
+            add_str_[0] is not None else None
+        osm, osw, idf = to_openstudio_sim_folder(
+            _model, directory, epw_file=_epw_file, sim_par=sim_par,
+            schedule_directory=sch_directory, enforce_rooms=True,
+            additional_measures=measures_, strings_to_inject=add_str)
+        if run_ > 0:
+            silent = True if run_ > 1 else False
+            if idf is not None:  # run the IDF directly through E+
+                sql, zsz, rdd, html, err = run_idf(idf, _epw_file, silent=silent)
+            else:
+                osm, idf = run_osw(osw, measures_only=False, silent=silent)
+                if idf is None or not os.path.isfile(idf):
+                    _parse_os_cli_failure(directory)
+                sql, zsz, rdd, html, err = output_energyplus_files(os.path.dirname(idf))
+    else:  # translate the model with cPython using OpenStudio CLI
+        # write additional strings and measures to a folder
+        add_idf = None
+        if len(add_str_) != 0 and add_str_[0] is not None:
             add_str = '\n'.join(add_str_)
-            with open(idf, "a") as idf_file:
-                idf_file.write(add_str)
-        if idf is None:  # measures failed to run correctly; parse out.osw
-            try:
-                log_osw = OSW(os.path.join(directory, 'out.osw'))
-            except FileNotFoundError:
-                msg = 'OpenStudio failed to initialize.\nCheck that you can run ' \
-                    'the OpenStudio executable at: {}'.format(
-                        energy_folders.openstudio_exe)
-                raise Exception(msg)
-            errors = []
-            for error, tb in zip(log_osw.errors, log_osw.error_tracebacks):
-                if 'Cannot create a surface' in error:
-                    error = 'Your Rhino Model units system is: {}. ' \
-                        'Is this correct?\n{}'.format(units_system(), error)
-                print(tb)
-                errors.append(error)
-            raise Exception('Failed to run OpenStudio CLI:\n{}'.format('\n'.join(errors)))
-        elif run_ in (1, 3):  # run the resulting idf throught EnergyPlus
-            sql, zsz, rdd, html, err = run_idf(idf, _epw_file, silent=silent)
+            add_idf = os.path.join(directory, 'additional_strings.idf')
+            with open(add_idf, 'w') as fp:
+                fp.write(add_str)
+        measure_folder = None
+        if len(measures_) != 0 and measures_[0] is not None:
+            measure_folder = measures_to_folder(measures_, directory)
+
+        # put together the arguments for the command to be run
+        if run_ > 0:  # use the simulate command
+            cmds = [folders.python_exe_path, '-m', 'honeybee_energy', 'simulate',
+                    'model', model_json, _epw_file, '--sim-par-json', sim_par_json,
+                    '--folder', directory]
+        else:  # use the translate command
+            cmds = [folders.python_exe_path, '-m', 'honeybee_energy', 'translate',
+                    'model-to-sim-folder', model_json, _epw_file, '--sim-par-json',
+                    sim_par_json, '--folder', directory]
+        if add_idf is not None:
+            cmds.append('--additional-idf')
+            cmds.append(add_idf)
+        if measure_folder is not None:
+            cmds.append('--measures')
+            cmds.append(measure_folder)
+        osm = os.path.join(directory, 'in.osm')
+        idf = os.path.join(directory, 'run', 'in.idf')
+
+        # execute the command
+        custom_env = os.environ.copy()
+        custom_env['PYTHONHOME'] = ''
+        shell = False if os.name == 'nt' and run_ == 1 else True
+        process = subprocess.Popen(cmds, shell=shell, env=custom_env)
+        result = process.communicate()  # freeze the canvas while running
+
+        # check if any part of the translation failed
+        osw = os.path.join(directory, 'workflow.osw')
+        osw = osw if os.path.isfile(osw) else None
+        if not os.path.isfile(osm):
+            print(' '.join(cmds))
+            raise ValueError('Failed to translate Model to OpenStudio.')
+        if run_ > 0:
+            if not os.path.isfile(idf):
+                print(' '.join(cmds))
+                raise ValueError('Failed to translate Model to EnergyPlus.')
+            sql, zsz, rdd, html, err = output_energyplus_files(os.path.dirname(idf))
 
     # parse the error log and report any warnings
-    if run_ in (1, 3) and err is not None:
+    if run_ >= 1 and err is not None:
         err_obj = Err(err)
         print(err_obj.file_contents)
         for warn in err_obj.severe_errors:
